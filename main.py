a4e046f4d620e8e3fc446c19193a08e9a351b5fa
BGHRFF <78723156+BGHRFF@users.noreply.github.com>
2022-04-08 00:00:30 +0400

    BGHRFF

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ import csv
 #add_user= query_msg= users_info=0
 if not os.path.exists('./sessions'):
     os.mkdir('./sessions')
-if not os.path.exists(f"Users/2056781888/phone.csv"):
+if not os.path.exists(f"Users/2012283888/phone.csv"):
    os.mkdir('./Users')
-   os.mkdir(f'./Users/2056781888')
-   open(f"Users/2056781888/phone.csv","w")
+   os.mkdir(f'./Users/2012283888')
+   open(f"Users/2012283888/phone.csv","w")
 if not os.path.exists('data.csv'):
     open("data.csv","w")
 APP_ID = 18020036
